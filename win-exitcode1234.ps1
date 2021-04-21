9b2fd9c47151c46fffc324a6290ebaabb047ac37
Gabby <54676731+MsGabsta@users.noreply.github.com>
2021-04-21 14:09:48 -0400

    Create win-exitcode1234.ps1

---
+++ win-exitcode1234.ps1
@@ -0,0 +1,6 @@
+# This script should get the current processes on a vm and list them in the text file configured.
+Start-Transcript
+echo "Hello World"
+Write-Host "Exiting with code 1234"
+Exit 1234
+Stop-Transcript
