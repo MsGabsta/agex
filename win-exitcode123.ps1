4bb8e3ee3bb0307e55f6057447bd529de677d835
Gabby <54676731+MsGabsta@users.noreply.github.com>
2021-04-21 17:30:25 -0400

    Update and rename win-exitcode1234.ps1 to win-exitcode123.ps1

--- win-exitcode123.ps1
+++ win-exitcode123.ps1
@@ -1,6 +1,6 @@
 # This script should get the current processes on a vm and list them in the text file configured.
 Start-Transcript
 echo "Hello World"
-Write-Host "Exiting with code 1234"
-Exit 1234
+Write-Host "Exiting with code 123"
+Exit 123
 Stop-Transcript
